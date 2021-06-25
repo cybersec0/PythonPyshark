84918662ae02f806d52d11acf164b4c70bc563f2
CYBERSEC <82158337+cybersec0@users.noreply.github.com>
2021-06-25 12:42:44 +0530

    Add files via upload

---
+++ pyshark-ospf.py
@@ -0,0 +1,8 @@
+              
+
+import pyshark
+
+capture = pyshark.LiveCapture ('eth0')
+for packet in capture:
+    if 'OSPF' in packet:
+        print('OSPF password: ' + packet.ospf.auth_simple)
